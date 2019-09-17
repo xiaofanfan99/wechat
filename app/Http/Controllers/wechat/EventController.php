98a85295c8a06158332fbf6603ad9e29b3913bc0
xiaofanfan99 <fhx2736298374@163.com>
2019-09-17 15:43:32 +0800

    aa

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Log;
 use const http\Client\Curl\Features\LARGEFILE;
-
+use DB;
 class EventController extends Controller
 {
     //接收微信发过来的消息[用户互动] 被动回复
@@ -21,6 +21,16 @@ class EventController extends Controller
         $xml_arr=(array)$xml_obj;
         \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
 //        echo $_GET['echostr'];
+        //业务逻辑
+        if($xml_arr['MsgType']=='event'){
+            //判断event=="subscribe"说明用户第一次扫码
+            if($xml_arr['Event']=='subscribe'){
+                //取出二维码专属推荐码 取第一个元素专属码
+                $share_code = explode('_',$xml_arr['EventKey'])[1];
+                //数据库share_code字段自增加一
+                DB::table('regist')->where(['regist_id'=>$share_code])->increment('share_code',1);
+            }
+        }
 
     }
 }
