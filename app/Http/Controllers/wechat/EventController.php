2e32f42a5f1179ac5b3af1b5e1bbf73f292197d6
xiaofanfan99 <fhx2736298374@163.com>
2019-09-28 10:44:01 +0800

    aa

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -135,17 +135,17 @@ class EventController extends Controller
                         'wechat_name'=>$user['nickname'],
                         'city'=>$user['city']
                     ]);
+                    $message='您好'.$user['nickname'].'当前时间:'.date('Y-m-d H:i:s',time());
+                    $xml_str='<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
+                    echo $xml_str;
+                }else{
+                    $message='欢迎回来'.$user['nickname'].'当前时间:'.date('Y-m-d H:i:s',time());
+                    $xml_str='<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
+                    echo $xml_str;
                 }
-                $message='您好'.$user['nickname'].'当前时间:'.date('Y-m-d H:i:s',time());
-                $xml_str='<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
-                echo $xml_str;
+
             }
         }
-        $user_info=file_get_contents("https://api.weixin.qq.com/cgi-bin/user/info?access_token=25_Zi7i6gL3NGquRfNxqbNVhWr4LonejMR3veK4HazVgaEg1-o_0ACh19NIU_UsJ18P9u03dWg7G2xkV4JpdyNUvELAvgrSC4oa3ck_Wd21QT-fivEulI2lEh_5X_mLxtgoT5-4bcWoyxlk7gffEDWhAAARQJ&openid=".$xml_arr['FromUserName']."&lang=zh_CN");
-        $user=json_decode($user_info,1);
-        $message='欢迎回来'.$user['nickname'].'当前时间:'.date('Y-m-d H:i:s',time());
-        $xml_str='<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
-        echo $xml_str;
 
         //业务逻辑
 //        if($xml_arr['MsgType']=='event'){
